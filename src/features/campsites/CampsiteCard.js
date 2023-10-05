4c0e453795513f6d1ccc13e4a4dbbd5ac52f6eb0
Katherine Ortega <keiry1851@gmail.com>
2023-10-05 07:07:02 -0700

    public

---
+++ src/features/campsites/CampsiteCard.js
@@ -0,0 +1,23 @@
+import {Card, CardImg, CardImgOverlay, CardTitle} from 'reactstrap';
+import { Link } from 'react-router-dom';
+
+const CampsiteCard = (props) => {
+    const { id, image, name } = props.campsite;
+    console.log(props.campsite)
+    return (
+        <Link to={`${id}`}>
+        <Card>
+            <CardImg
+                width='100%'
+                src={image}
+                alt={name}
+            />
+            <CardImgOverlay>
+            <CardTitle>{name}</CardTitle>
+            </CardImgOverlay>
+        </Card>
+        </Link>
+    );
+}
+
+export default CampsiteCard;
